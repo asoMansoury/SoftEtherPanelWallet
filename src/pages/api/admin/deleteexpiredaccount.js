20582192f30aa6e3124423aeb6dd074a729c06c8
unknown <aso.mansoury@gmail.com>
2023-08-15 12:35:45 -0400

    add calledTime to deleteExpiredAccounts

--- src/pages/api/admin/deleteexpiredaccount.js
+++ src/pages/api/admin/deleteexpiredaccount.js
@@ -11,7 +11,8 @@ export default async function handler(req,res){
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.status(200).json({
             users:deleteExpiredUsersJob,
-            tests:deleteExpiredTestedUsersJob
+            tests:deleteExpiredTestedUsersJob,
+            calledTime:new Date()
         });
     } 
 }
