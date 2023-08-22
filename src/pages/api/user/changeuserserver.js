f29e79ffa64d50bf356de6fd7c82084a5739720c
unknown <aso.mansoury@gmail.com>
2023-08-22 15:28:02 -0400

    Complete removing user from softethr

--- src/pages/api/user/changeuserserver.js
+++ src/pages/api/user/changeuserserver.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
         }
       // Handle the POST request here
       const { body } = req.body;
-      const selectedServer =await GetServerByCode(body.servercode);
+
       var result =await ChangeUserServer(body);
       const newSelectedServer = await GetServerByCode(result.currentservercode);
       if(result==null){
