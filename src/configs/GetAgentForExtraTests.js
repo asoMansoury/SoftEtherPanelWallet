f1d8cfa65ad04cce81f98d8aa78d3c7f0cfbbb2c
unknown <aso.mansoury@gmail.com>
2024-05-01 21:21:46 -0400

    change email test to lowercase

--- src/configs/GetAgentForExtraTests.js
+++ src/configs/GetAgentForExtraTests.js
@@ -14,7 +14,9 @@ export function GetAgentForExtraTests(email){
     var result = {
         isValid:false,
     }
-    var agent = agentEmails.filter(z=>z.email==email)[0];
+    var emailStr = email.toString().trim().toLocaleLowerCase();
+    console.log(emailStr)
+    var agent = agentEmails.filter(z=>z.email.trim().toLocaleLowerCase()==emailStr)[0];
     if(agent!=null){
         result.isValid=true;
     }
